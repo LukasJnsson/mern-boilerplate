2d299a54ca6354faafae6d72d2b0ecc7629063cf
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2023-09-21 12:29:44 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './styles/App.css';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        React + Redux
+      </header>
+    </div>
+  );
+};
